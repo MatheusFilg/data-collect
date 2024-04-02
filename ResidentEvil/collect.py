eb1efe5b8be1bb984865a4c5efe213ce1bed1253
Matheus F <matheus_falmeida@hotmail.com>
2024-04-02 15:57:44 -0300

    refactor the base code to functions

--- ResidentEvil/collect.py
+++ ResidentEvil/collect.py
@@ -2,9 +2,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.residentevildatabase.com/personagens/ada-wong/"
-
-headers = {
+def get_content(url):
+    headers = {
     'authority': 'www.residentevildatabase.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
@@ -21,29 +20,29 @@ headers = {
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 OPR/107.0.0.0',
 }
+    resp = requests.get(url, headers=headers)
+    return resp
 
-resp = requests.get(url, headers=headers)
-# %%
-resp.status_code
-# %%
-soup = BeautifulSoup(resp.text)
-soup
-# %%
-div_page = soup.find("div", class_="td-page-content")
-div_page
-# %%
-paragrafo = div_page.find_all("p")[1]
-paragrafo
-# %%
-linhas = paragrafo.find_all("em")
-linhas
-# %%
-data = {}
+def get_basic_infos(soup):
+    div_page = soup.find("div", class_="td-page-content")
+    paragrafo = div_page.find_all("p")[1]
+    linhas = paragrafo.find_all("em")
+    
+    data = {}
+    
+    for i in linhas:
+        chave, valor = i.text.split(":")
+        chave = chave.strip(" ")
+        data[chave] = valor.strip(" ")
 
-for i in linhas:
-    chave, valor = i.text.split(":")
-    chave = chave.strip(" ")
-    data[chave] = valor.strip(" ")
-
-data
+    return data
+# %%
+url = "https://www.residentevildatabase.com/personagens/ada-wong/"
+resp = get_content(url)
+# %%
+if resp.status_code != 200:
+    print('Não foi possível realizar a requisição')
+else:
+    soup = BeautifulSoup(resp.text)
+    get_basic_infos(soup)
 # %%
